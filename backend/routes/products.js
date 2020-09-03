04b61a69571d58d6599e02d98237ec0114d7ae68
fouad97 <loukrichi21@gmail.com>
2020-09-03 17:33:21 +0100

    added express-validator to validate products

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,26 +2,45 @@ const express = require("express");
 const router = express.Router();
 
 const { Product } = require("../model/products");
+const { productValidator } = require("../validators/auth");
+const { runValidation } = require("../validators");
 
 router.get("/", async (req, res) => {
 	const product = await Product.find().sort("_id");
 	res.send(product);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", productValidator, runValidation, async (req, res) => {
+	const {
+		_id,
+		name,
+		category,
+		price,
+		image,
+		brand,
+		rating,
+		numReviews,
+	} = req.body;
 	const product = new Product({
-		_id: 4,
-		name: "Best Pants",
-		category: "Pants",
-		image: "/images/p2.jpg",
-		price: 70,
-		brand: " Nike",
-		rating: 4.5,
-		numReviews: 8,
+		_id,
+		name,
+		category,
+		image,
+		price,
+		brand,
+		rating,
+		numReviews,
 	});
 
 	const result = await product.save();
 	res.send(result);
+});
+
+router.delete("/:id", async (req, res) => {
+	const product = await Product.findByIdAndDelete(req.body._id);
+	if (!product) return res.status(404).send("Product not found");
+
+	res.send(product)
 });
 
 module.exports = router;
---
+++ backend/validators/auth.js
@@ -0,0 +1,30 @@
+const { check } = require("express-validator");
+
+exports.productValidator = [
+	check("_id").isInt({min: 0, max: 99}).not().isEmpty().withMessage("ID is required"),
+	check("name").isString().not().isEmpty().withMessage("name is required"),
+	check("category")
+		.isString()
+		.not()
+		.isEmpty()
+		.withMessage("Category is required"),
+	check("image").isString().not().isEmpty().withMessage("Image is required"),
+	check("price").not().isEmpty().withMessage("Price is required"),
+	check("brand").isString().not().isEmpty().withMessage("Brans is required"),
+	check("rating")
+		.isInt({ min: 1, max: 5 })
+		.not()
+		.isEmpty()
+		.withMessage("Rating must be 1-5"),
+	check("numReviews").not().isEmpty().withMessage("Review is required"),
+];
+
+/**
+ * 	name: "Best Pants",
+		category: "Pants",
+		image: "/images/p2.jpg",
+		price: 70,
+		brand: " Nike",
+		rating: 4.5,
+		numReviews: 8,
+ */
